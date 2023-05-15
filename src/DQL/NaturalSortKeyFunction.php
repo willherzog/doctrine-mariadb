998414be65cfedec8ef8ba279bc24dde84da6362
willherzog <willherzog@gmail.com>
2023-05-15 13:12:11 -0600

    Update NaturalSortKeyFunction.php
    Use Parser->StateFieldPathExpression() and call dispatch() method of its output in the getSql() method

--- src/DQL/NaturalSortKeyFunction.php
+++ src/DQL/NaturalSortKeyFunction.php
@@ -3,7 +3,6 @@
 namespace WHDoctrine\DQL;
 
 use Doctrine\ORM\Query\AST\Functions\FunctionNode;
-use Doctrine\ORM\Query\AST\Subselect;
 use Doctrine\ORM\Query\Lexer;
 use Doctrine\ORM\Query\Parser;
 use Doctrine\ORM\Query\SqlWalker;
@@ -15,20 +14,20 @@ use Doctrine\ORM\Query\SqlWalker;
  */
 class NaturalSortKeyFunction extends FunctionNode
 {
-	protected $orderByItem;
+	protected $pathExpression;
 
 	public function parse(Parser $parser): void
 	{
 		$parser->match(Lexer::T_IDENTIFIER);
 		$parser->match(Lexer::T_OPEN_PARENTHESIS);
 
-		$this->orderByItem = $parser->OrderByItem();
+		$this->pathExpression = $parser->StateFieldPathExpression();
 
 		$parser->match(Lexer::T_CLOSE_PARENTHESIS);
 	}
 
 	public function getSql(SqlWalker $sqlWalker): string
 	{
-		return 'NATURAL_SORT_KEY(' . $this->orderByItem . ')';
+		return 'NATURAL_SORT_KEY(' . $this->pathExpression->dispatch($sqlWalker) . ')';
 	}
 }
